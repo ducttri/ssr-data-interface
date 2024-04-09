e8c353fc06863b54f90e6db3f37f9a82c1f45852
trinh127 <trinh127@umn.edu>
2024-04-08 19:10:36 -0500

    implement quicklook

--- src/app/quicklook/page.tsx
+++ src/app/quicklook/page.tsx
@@ -4,6 +4,7 @@ import { Box, Button, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import Plot from "react-plotly.js";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -17,8 +18,79 @@ const VisuallyHiddenInput = styled("input")({
   width: 1,
 });
 
+interface JSONData {
+  timestamp: number;
+  x123: {
+    accumulation_time: number;
+    board_temp: number;
+    det_high_voltage: number;
+    det_temp: number;
+    fast_counts: number;
+    real_time: number;
+    slow_counts: number;
+  };
+  x1: {
+    arm_temp: number;
+    counts: number;
+    dead_time: number;
+    real_time: number;
+    sipm_operating_voltage: number;
+    sipm_target_voltage: number;
+    sipm_temp: number;
+  };
+  c1: {
+    arm_temp: number;
+    counts: number;
+    dead_time: number;
+    real_time: number;
+    sipm_operating_voltage: number;
+    sipm_target_voltage: number;
+    sipm_temp: number;
+  };
+  m1: {
+    arm_temp: number;
+    counts: number;
+    dead_time: number;
+    real_time: number;
+    sipm_operating_voltage: number;
+    sipm_target_voltage: number;
+    sipm_temp: number;
+  };
+  m5: {
+    arm_temp: number;
+    counts: number;
+    dead_time: number;
+    real_time: number;
+    sipm_operating_voltage: number;
+    sipm_target_voltage: number;
+    sipm_temp: number;
+  };
+}
+
+
 export default function QuickLook() {
   const [fileContent, setFileContent] = useState<string | null>(null);
+  const [data, setData] = useState<{ x: number[]; y: number[] }>({
+    x: [],
+    y: [],
+  });
+  let currentDate: Date;
+
+  function insertionSort(x: number[], y: number[]) : void {
+    for (let i: number = 1; i < x.length; i ++) {
+      let key: number = x[i];
+      let key2: number = y[i];
+      let j: number = i - 1;
+      while (j >= 0 && x[j] > key) {
+        x[j + 1] = x[j];
+        y[j + 1] = y[j];
+        j = j - 1;
+      }
+      x[j+1] = key;
+      y[j+1] = key2;
+    }
+    console.log(x);
+  }
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -28,8 +100,20 @@ export default function QuickLook() {
       reader.onload = (e: ProgressEvent<FileReader>) => {
         if (e.target?.result) {
           try {
-            const json = JSON.parse(e.target.result as string);
-            setFileContent(JSON.stringify(json, null, 2)); // Pretty print the JSON
+            const json: JSONData[] = JSON.parse(e.target.result as string);
+
+            let xData = json.map((item) => item.timestamp);
+            let yData = json.map((item) => item.x123.board_temp);
+
+            insertionSort(xData, yData);
+            const starttime = xData[0];
+            xData = xData.map((item) => item - starttime);
+            currentDate = new Date(starttime*1000);
+
+            setData({ x: xData, y: yData });
+            setFileContent(currentDate.toLocaleString()); // Pretty print the JSON
+
+            // setFileContent(JSON.stringify(json[0].timestamp, null, 2)); // Pretty print the JSON
           } catch (error) {
             console.error("Error parsing JSON:", error);
             setFileContent("Error reading the file");
@@ -64,6 +148,10 @@ export default function QuickLook() {
           style={{ whiteSpace: "pre-wrap", marginTop: "20px" }}
         >
           {fileContent}
+          <Plot
+            data={[{ x: data.x, y: data.y, type: "scatter" }]}
+            layout={{ width: 600, height: 400, title: "Board Temperature vs. Time" }}
+          />
         </Typography>
       )}
     </Box>
