b125fbc70f0ece4485f190b620687e7e2331cd35
trinh127 <trinh127@umn.edu>
2024-07-03 16:02:17 -0500

    updated health table graph dimension

--- src/components/HealthGraph/GraphsList.tsx
+++ src/components/HealthGraph/GraphsList.tsx
@@ -5,10 +5,11 @@ import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { Box, Grid, Tab } from "@mui/material";
 import { useState } from "react";
 import PlotlyGraph from "./PlotlyGraph";
-import Zoom from "@mui/material/Zoom";
+
+type detect = "c1" | "m1" | "m5" | "x1" | "x123"
 
 export default function GraphList({ data }: { data: HealthJSONData }) {
-  const [detector, setDetector] = useState("1");
+  const [detector, setDetector] = useState<detect>("c1");
 
   const utcDates: string[] = data.raw_data.timestamp.map(
     (timestamp: number) => {
@@ -20,27 +21,182 @@ export default function GraphList({ data }: { data: HealthJSONData }) {
     event: React.SyntheticEvent,
     newValue: string
   ) => {
-    setDetector(newValue);
+    setDetector(newValue as detect);
   };
 
   return (
     <Grid>
-      <Box sx={{ width: "100%", typography: "body1" }}>
+      <Box
+        sx={{
+          width: "100%",
+          typography: "body1",
+          borderRadius: 1,
+          boxShadow: 1,
+        }}
+      >
         <TabContext value={detector}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList
               onChange={handleDetectorChange}
               aria-label="lab API tabs example"
             >
-              <Tab label="C1" value="1" />
-              <Tab label="M1" value="2" />
-              <Tab label="M5" value="3" />
-              <Tab label="X1" value="4" />
-              <Tab label="X123" value="5" />
+              <Tab label="C1" value="c1" />
+              <Tab label="M1" value="m1" />
+              <Tab label="M5" value="m5" />
+              <Tab label="X1" value="x1" />
+              <Tab label="X123" value="x123" />
             </TabList>
           </Box>
 
-          <TabPanel value="1">
+          {detector == "x123" ? (
+            <Grid container columns={2}>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.board_temp.value}
+                  xLabel={"Time"}
+                  yLabel={"DP5 board temperature"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.board_temp.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.det_high_voltage.value}
+                  xLabel={"Time"}
+                  yLabel={"Detector high voltage"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.det_high_voltage.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.det_temp.value}
+                  xLabel={"Time"}
+                  yLabel={"Detector head temperature"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.det_temp.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.fast_counts.value}
+                  xLabel={"Time"}
+                  yLabel={"Fast sharper # of counts"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.fast_counts.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.slow_counts.value}
+                  xLabel={"Time"}
+                  yLabel={"Slow sharper $ of counts"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.slow_counts.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.accumulation_time.value}
+                  xLabel={"Time"}
+                  yLabel={"Accumulation Time"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.accumulation_time.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data.x123.real_time.value}
+                  xLabel={"Time"}
+                  yLabel={"Real Time"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data.x123.real_time.unit}
+                />
+              </Grid>
+            </Grid>
+          ) : (
+            <Grid container columns={2}>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].arm_temp.value}
+                  xLabel={"Time"}
+                  yLabel={"ARM processor temperature"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].arm_temp.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].sipm_temp.value}
+                  xLabel={"Time"}
+                  yLabel={"SiPM board temperature"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].sipm_temp.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].sipm_operating_voltage.value}
+                  xLabel={"Time"}
+                  yLabel={"SiPM operating voltage"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].sipm_operating_voltage.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].sipm_target_voltage.value}
+                  xLabel={"Time"}
+                  yLabel={"SiPM target voltage"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].sipm_target_voltage.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].counts.value}
+                  xLabel={"Time"}
+                  yLabel={"Counts"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].counts.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].dead_time.value}
+                  xLabel={"Time"}
+                  yLabel={"Dead Time"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].dead_time.unit}
+                />
+              </Grid>
+              <Grid item xs={1} id="parent-container">
+                <PlotlyGraph
+                  xData={utcDates}
+                  yData={data.raw_data[detector].real_time.value}
+                  xLabel={"Time"}
+                  yLabel={"Real Time"}
+                  xUnit={"UTC"}
+                  yUnit={data.raw_data[detector].real_time.unit}
+                />
+              </Grid>
+            </Grid>
+          )}
+
+          {/* <TabPanel value="1">
             <Grid container columns={2}>
               <Grid item xs={1} id="parent-container">
                 <PlotlyGraph
@@ -413,7 +569,7 @@ export default function GraphList({ data }: { data: HealthJSONData }) {
                 />
               </Grid>
             </Grid>
-          </TabPanel>
+          </TabPanel> */}
         </TabContext>
       </Box>
     </Grid>
