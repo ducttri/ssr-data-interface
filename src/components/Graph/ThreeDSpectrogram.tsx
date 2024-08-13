bfd4cfd21e000c10ff17cc587e6692914bcc1136
ducttri <trinhtduc512@gmail.com>
2024-08-13 10:37:25 -0500

    speed up render for stepgraph and spectrogram

--- src/components/Graph/ThreeDSpectrogram.tsx
+++ src/components/Graph/ThreeDSpectrogram.tsx
@@ -1,46 +1,48 @@
 import React from "react";
-import dynamic from "next/dynamic";
+import Plot from "react-plotly.js";
+import { baselightTheme } from "@/utils/theme/DefaultColors";
 
-const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
-
-export const ThreeDSpectrogram = ({
-  xData,
-  yData,
-  zData,
-  xLabel,
-  yLabel,
-  title,
-}: {
+interface ThreeDSpectrogramProps {
   xData: any[];
   yData: any[];
   zData: any[];
   xLabel: string;
   yLabel: string;
   title: string;
-}) => {
-  return (
-    <Plot
-      data={[
-        {
-          x: xData,
-          y: yData,
-          z: zData,
-          type: "heatmap",
-        },
-      ]}
-      layout={{
-        autosize: true, // Enable responsive sizing
-        title: title,
-        xaxis: { title: xLabel },
-        yaxis: { title: yLabel },
-        font: {
-          family:
-            "'__Plus_Jakarta_Sans_a182b8','__Plus_Jakarta_Sans_Fallback_a182b8',Helvetica,Arial,sans-serif",
-        },
-      }}
-      config={{ responsive: true }}
-      useResizeHandler={true} // Important: This tells Plotly to re-render on resize
-      style={{ width: "100%", height: "100%", aspectRatio: "4/3" }} // Ensure the div containing the plot is fully responsive
-    />
-  );
-};
+}
+
+export class ThreeDSpectrogram extends React.Component<ThreeDSpectrogramProps> {
+  constructor(props: ThreeDSpectrogramProps) {
+    super(props);
+  }
+
+  render() {
+    const { xData, yData, zData, xLabel, yLabel, title } = this.props;
+
+    return (
+      <Plot
+        data={[
+          {
+            x: xData,
+            y: yData,
+            z: zData,
+            type: "heatmap",
+          },
+        ]}
+        layout={{
+          autosize: true, // Enable responsive sizing
+          title: title,
+          xaxis: { title: xLabel },
+          yaxis: { title: yLabel },
+          font: {
+            family:
+              "'__Plus_Jakarta_Sans_a182b8','__Plus_Jakarta_Sans_Fallback_a182b8',Helvetica,Arial,sans-serif",
+          },
+        }}
+        config={{ responsive: true }}
+        useResizeHandler={true} // Important: This tells Plotly to re-render on resize
+        style={{ width: "100%", height: "100%", aspectRatio: "4/3" }} // Ensure the div containing the plot is fully responsive
+      />
+    );
+  }
+}
